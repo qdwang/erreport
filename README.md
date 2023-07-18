7167d2fd10b1e80bbadf3e546ccb27d0058ee590
qdwang <wangqidong@gmail.com>
2023-07-18 12:01:36 +0800

    update doc; bump version to v0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # erreport
 A `Result` helper for Rust
+
+## How to use
+```rust
+erreport::gen_report_code!(); // This will create a mod called `report` in current scope.
+use report::{Report, ToReport};
+
+fn test() -> Result<(), Report> {
+    any_result_impl_std_error_Error.to_report()?;
+    Ok(())
+}
+```
+## Visibility
+```rust
+pub(crate) mod report {
+    pub Report;
+    pub(crate) ToReport;
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,20 @@
 //! 
 //! ## How to use
 //! ```rust
-//! erreport::gen_report_code!();
+//! erreport::gen_report_code!(); // This will create a mod called `report` in current scope.
 //! use report::{Report, ToReport};
+//!
+//! fn test() -> Result<(), Report> {
+//!     any_result_impl_std_error_Error.to_report()?;
+//!     Ok(())
+//! }
+//! ```
+//! ## Visibility
+//! ```rust
+//! pub(crate) mod report {
+//!     pub Report;
+//!     pub(crate) ToReport;
+//! }
 //! ```
 
 /// This will create a mod called `report` in current scope.
